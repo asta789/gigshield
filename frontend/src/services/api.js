d97f33d04530cff9a88095330a9d74720c3a2d5c
asta789 <dhanrajsinghyadav2@gmail.com>
2026-03-20 18:39:42 +0530

    fix backend for render3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://gigshield-backend-egry.onrender.com' });
+const api = axios.create({ baseURL: 'https://gigshield1.onrender.com' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
